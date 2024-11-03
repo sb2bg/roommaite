d475d8b0c3a53956337f5c7490f79450b73a08d1
sb2bg <sullivanbognar@gmail.com>
2024-11-03 04:55:56 -0600

    Refactor Question class to expose answer property and implement fromMap method for deserialization

--- lib/models/questions.dart
+++ lib/models/questions.dart
@@ -1,22 +1,38 @@
 abstract class Question<T> {
   final String _question;
-  final T? _answer;
+  T? answer;
 
-  Question(this._question, [this._answer]);
+  Question(this._question, [this.answer]);
 
   String get question => _question;
-  T? get answer => _answer;
 
-  void toMap();
+  Map<String, String?> toMap();
+
+  static Question fromMap(Map<String, dynamic> map) {
+    if (map['answer'] is String && map['answer'] != null) {
+      if (map['answer'] == 'Yes' || map['answer'] == 'No') {
+        return YesNoQuestion(map['question'], map['answer'] == 'Yes');
+      } else {
+        return OpenEndedQuestion(map['question'], map['answer']);
+      }
+    } else {
+      return OpenEndedQuestion(map['question']);
+    }
+  }
 }
 
 class YesNoQuestion extends Question<bool> {
   YesNoQuestion(super.question, [super.answer]);
 
   @override
-  Map<String, bool?> toMap() {
+  Map<String, String?> toMap() {
     return {
-      question: answer,
+      'question': question,
+      'answer': answer == null
+          ? null
+          : answer == true
+              ? 'Yes'
+              : 'No',
     };
   }
 }
@@ -27,7 +43,8 @@ class OpenEndedQuestion extends Question<String> {
   @override
   Map<String, String?> toMap() {
     return {
-      question: answer,
+      'question': question,
+      'answer': answer,
     };
   }
 }
