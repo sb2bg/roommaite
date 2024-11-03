fef77016ff6d254674ccd4bb89450a6da63e588f
sb2bg <sullivanbognar@gmail.com>
2024-11-03 04:56:05 -0600

    Refactor SearchPage to StatefulWidget and implement match fetching in initState

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:roommaite/models/profile.dart';
+import 'package:roommaite/providers/auth_provider.dart';
+import 'package:roommaite/util/vector_data_helper.dart';
 import 'package:roommaite/widgets/question_page.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  List<Profile>? matches;
+
+  @override
+  void initState() {
+    super.initState();
+    final authService = Provider.of<AuthService>(context, listen: false);
+    IrisVectorDataHelper.getMatches(authService).then((value) {
+      setState(() {
+        matches = value;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,9 +32,7 @@ class SearchPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Search Page'),
       ),
-      body: const Center(
-        child: QuestionPage(edit: false)
-      ),
+      body: const Center(child: QuestionPage(edit: false)),
     );
   }
 }
