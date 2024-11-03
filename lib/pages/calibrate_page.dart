6fc18862513bea2dd1708a79b855c0b88e99033c
sb2bg <sullivanbognar@gmail.com>
2024-11-02 21:42:40 -0500

    Add CalibratePage widget with basic layout

---
+++ lib/pages/calibrate_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CalibratePage extends StatefulWidget {
+  const CalibratePage({super.key});
+
+  @override
+  State<CalibratePage> createState() => _CalibratePageState();
+}
+
+class _CalibratePageState extends State<CalibratePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calibrate Page'),
+      ),
+      body: Center(
+        child: const Text('Calibrate Page'),
+      ),
+    );
+  }
+}
